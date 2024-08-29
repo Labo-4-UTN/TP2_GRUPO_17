4eb01569a90f55d62cfc209a2fd1964a52c9fe43
BasiliscX <juniornavarro1988@gmail.com>
2024-08-29 20:35:46 -0300

    1.8 added 5 profesores

--- src/EJERCICIO1/mainEjercicio1_b.java
+++ src/EJERCICIO1/mainEjercicio1_b.java
@@ -3,8 +3,9 @@ package EJERCICIO1;
 public class mainEjercicio1_b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Profesor P1 = new Profesor("Laura", 23," Preceptor ",9);
+		Profesor P2 = new Profesor("Lorena", 32," ViceDirector ",20);
+		Profesor P3 = new Profesor("Leonardo", 43," Profesor ",11);
+		Profesor P4 = new Profesor("Lina", 34," Profesor ",5);
+		Profesor P5 = new Profesor("Lucas", 53," Preceptor ",7);	}
 }
