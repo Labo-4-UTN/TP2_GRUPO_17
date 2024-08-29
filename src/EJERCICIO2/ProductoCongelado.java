3a16f46dc309773034932d2f5e790aba7385d7a8
AgustinaVillalba <villalbaagus04@gmail.com>
2024-08-29 20:02:19 -0300

    agregue los constructores y los getters a las clases de ProductoCongelado y ProductoFresco

--- src/EJERCICIO2/ProductoCongelado.java
+++ src/EJERCICIO2/ProductoCongelado.java
@@ -4,4 +4,17 @@ public class ProductoCongelado extends Producto{
 	
 	private float tempRec;
 
+	public float getTempRec() {
+		return tempRec;
+	}
+
+	public ProductoCongelado() {
+		super();
+	}
+	
+	public ProductoCongelado(String fecha_de_caducidad, int numeroLote, float tempRec) {
+		super(fecha_de_caducidad, numeroLote);
+		this.tempRec = tempRec;
+	}
+
 }
--- src/EJERCICIO2/ProductoFresco.java
+++ src/EJERCICIO2/ProductoFresco.java
@@ -5,4 +5,25 @@ public class ProductoFresco extends Producto {
 	String FechaEnvasado;
 	String PaisOrigen;
 	
+	
+	public String getFechaEnvasado() {
+		return FechaEnvasado;
+	}
+
+	public String getPaisOrigen() {
+		return PaisOrigen;
+	}
+	
+	
+
+	public ProductoFresco() {
+		super();
+	}
+
+	public ProductoFresco(String fecha_de_caducidad, int numeroLote, String fechaEnvasado, String paisOrigen) {
+		super(fecha_de_caducidad, numeroLote);
+		FechaEnvasado = fechaEnvasado;
+		PaisOrigen = paisOrigen;
+	}
+	
 }
