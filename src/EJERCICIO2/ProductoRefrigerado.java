6a5824023214a09a4c39ac781f569a03287e90c7
Nac370 <este370@outlook.com>
2024-08-29 17:35:14 -0300

    Realizada tabla 2.3
    Clase ProductoRefrigerado

---
+++ src/EJERCICIO2/ProductoRefrigerado.java
@@ -0,0 +1,33 @@
+package EJERCICIO2;
+
+public class ProductoRefrigerado extends Producto{
+	
+	private String codAlim;
+
+	//Constructores
+	public ProductoRefrigerado() {
+		super();
+		this.codAlim = "000000";
+	}
+	
+	public ProductoRefrigerado(String fecha_de_caducidad, int numeroLote, String cod) {
+		super(fecha_de_caducidad, numeroLote);
+		this.codAlim = cod;
+	}
+	
+	
+	//Getters
+	public String getCodAlim() {
+		return codAlim;
+	}
+
+	//Setters
+	public void setCodAlim(String codAlim) {
+		this.codAlim = codAlim;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "ProductoRefrigerado [codAlim=" + codAlim + "]";
+	}
+}
