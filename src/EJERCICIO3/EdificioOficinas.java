ecd3afc7d6a2acfe22e5ca85dd3797600716eab9
BasiliscX <juniornavarro1988@gmail.com>
2024-08-30 14:23:50 -0300

    3.7 added constructors in EdificioOficinas

--- src/EJERCICIO3/EdificioOficinas.java
+++ src/EJERCICIO3/EdificioOficinas.java
@@ -4,6 +4,20 @@ public class EdificioOficinas extends Edificio {
 
 	int cantidadDeOficinas;
 	
+	public EdificioOficinas() {
+		super();
+	}	
+	
+	public EdificioOficinas(int cantidadDeOficinas) {
+		super();
+		this.cantidadDeOficinas = cantidadDeOficinas;
+	}
+
+	public EdificioOficinas(int longitud, int ancho, int cantidadDeOficinas) {
+		super(longitud, ancho);
+		this.cantidadDeOficinas = cantidadDeOficinas;
+	}
+
 	public int getCantidadDeOficinas() {
 		return this.cantidadDeOficinas;
 	}
