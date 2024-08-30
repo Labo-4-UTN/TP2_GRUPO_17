0fc947478afe568e76e39dc3fef22befd2a0dce7
BasiliscX <juniornavarro1988@gmail.com>
2024-08-30 14:13:14 -0300

    Create EdificioOficinas.java

---
+++ src/EJERCICIO3/EdificioOficinas.java
@@ -0,0 +1,10 @@
+package EJERCICIO3;
+
+public class EdificioOficinas extends Edificio {
+
+	int cantidadDeOficinas;
+	
+	public int getCantidadDeOficinas() {
+		return this.cantidadDeOficinas;
+	}
+}
