316e3f4a32693dd4d9caa723b60abd93de60e7ec
AgustinaVillalba <villalbaagus04@gmail.com>
2024-08-29 21:19:44 -0300

    agregue atributos, constructores y demas en la clase polideportivo

--- src/EJERCICIO3/Polideportivo.java
+++ src/EJERCICIO3/Polideportivo.java
@@ -2,6 +2,32 @@ package EJERCICIO3;
 
 public class Polideportivo extends Edificio implements Instalacion{
 
+	private String Nombre;
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+
+	public Polideportivo() {
+		super();
+	}
+
+	public Polideportivo(int longitud, int ancho, String nombre) {
+		super(longitud, ancho);
+		this.Nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Polideportivo Nombre= " + Nombre + " Longitud= " + super.getLongitud() +" Ancho= "+ super.getAncho();
+	}
+
+	
 	
 	
 }
